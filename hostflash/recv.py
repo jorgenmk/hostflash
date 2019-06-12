af3eb4a1c9a0ab20fa396701b38fd92c1b574ac1
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2019-06-12 15:00:53 +0200

    asyncio

--- hostflash/recv.py
+++ hostflash/recv.py
@@ -3,57 +3,48 @@ import subprocess
 import sys
 import serial
 import threading
+import asyncio
 
 SEGGER = sys.argv[1]
 SERIAL_PORT=sys.argv[2]
 
-def serial_reader(port):
-	s = socket.socket()
-	s.bind("", 12346)
-	s.listen(5)
-	ser = serial.Serial(port, 115200, timeout=1)
-	while True:
-		conn, addr = s.accept()
-		while True:
-			data = ser.readline()
-			if data:
-				try:
-					conn.send(data)
-				except Exception:
-					break
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+async def recv_hex(reader, writer):
+    filename='foo.hex'
+    f = open(filename,'wb')
+
+    while True:
+        data = await reader.read(2048)
+        if data:
+            f.write(data)
+        else:
+            break
+    f.close()
+    cmd = "nrfjprog --program foo.hex --sectorerase --reset".split()
+    subprocess.run(cmd)
+    print("Done")
+
+async def serial_send(reader, writer):
+    ser = serial.Serial(SERIAL_PORT, 115200, timeout=1)
+    while True:
+        data = ser.readline()
+        if data:
+            try:
+                writer.write(data)
+            except Exception:
+                break
 
 def main():
-	t = threading.Thread(target=serial_reader, args=())
-	t.start()
-	port = 12345
-	s = socket.socket()
-	host = 'localhost'
-	s.bind((host, port))
-	s.listen(5)
-
-	print ('Server listening....')
-
-	while True:
-		try:
-			conn, addr = s.accept()     # Establish connection with client.
-		except KeyboardInterrupt:
-			exit()
-		print ('Got connection from', addr)
-
-		filename='foo.hex'
-		f = open(filename,'wb')
-
-		while True:
-			data = conn.recv(1024)
-			if data:
-				f.write(data)
-			else:
-				break
-		f.close()
-		cmd = "nrfjprog -f nrf91 --program foo.hex --sectorerase --reset".split()
-		subprocess.run(cmd)
-		print("Done")
-		conn.close()
+    loop = asyncio.get_event_loop()
+    server1 = loop.run_until_complete(asyncio.start_server(recv_hex, '127.0.0.1', 12345, loop=loop))
+    server2 = loop.run_until_complete(asyncio.start_server(serial_send, '127.0.0.1', 12346, loop=loop))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        server1.close()
+        server2.close()
 
 if __name__ == '__main__':
     main()
